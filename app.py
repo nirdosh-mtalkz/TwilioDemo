624253981b84476a52c8674d0c8b457170effe1c
nirdosh-mtalkz <nirdosh.mtalkz@gmail.com>
2023-01-09 17:41:38 +0530

    getting specific record from logs

--- app.py
+++ app.py
@@ -11,20 +11,20 @@ auth_token = config['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
 
-# message = client.messages.create(
-#   to="+918081701067",
-#   from_="+16086556107",
-#   body = "Hello Testing from trial phone number......"
-# )
-#print(message.sid)
+message = client.messages.create(
+  to="+918081701067",
+  from_="+16086556107",
+  body = "Hello Testing from trial phone number......"
+)
+print(message.sid)
 
 
-# call = client.calls.create(
-#   to="+918081701067",
-#   from_="+16086556107",
-#   url="https://demo.twilio.com/welcome/voice/voice.xml"
-# )
-#print(call.sid)
+call = client.calls.create(
+  to="+918081701067",
+  from_="+16086556107",
+  url="https://demo.twilio.com/welcome/voice/voice.xml"
+)
+print(call.sid)
 
 
 print("Call Records")
@@ -34,3 +34,11 @@ for call in client.calls.list():
 print("Messages Records")
 for sms in client.messages.list():
     print("%s %s"%(sms.to,sms.from_))
+
+
+
+message = client.messages("SM5160f0f39132300f91cba9dec8b01ba7").fetch()
+print("%s %s"%(message.to,message.from_))
+
+call = client.calls("CAb278e86b425ce30516070e17d17bc273").fetch()
+print("%s %s"%(call.to,call.from_))
